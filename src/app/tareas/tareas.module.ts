77c592514959633c88253ca63123667cef957fa7
Sergioess <sergioess@hotmail.com>
2021-11-22 20:37:37 -0500

    importa angularmaterial en tarea.module

--- src/app/tareas/tareas.module.ts
+++ src/app/tareas/tareas.module.ts
@@ -7,18 +7,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from "@angular/forms";
 import { TareaDetailComponent } from './components/tarea-detail/tarea-detail.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatIconModule } from '@angular/material/icon'
-import { MatListModule } from '@angular/material/list';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
+
 
 
 
@@ -35,17 +27,7 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
-    MatCardModule,
-    MatSliderModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatCheckboxModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatBadgeModule,
-    MatTooltipModule,
-    MatDialogModule,
+    AngularMaterialModule,
     MatDatepickerModule,
     MatNativeDateModule,
   ],
